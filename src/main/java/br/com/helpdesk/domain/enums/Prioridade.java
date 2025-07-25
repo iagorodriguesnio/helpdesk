df1bc489c14b4d5f02a169304d7f48f96d64199f
Iago Rodrigues <iagorodriguesn.io@gmail.com>
2025-07-24 22:24:50 -0300

    Adicionando o Enums

---
+++ src/main/java/br/com/helpdesk/domain/enums/Prioridade.java
@@ -0,0 +1,35 @@
+package br.com.helpdesk.domain.enums;
+
+public enum Perfil {
+
+    ADMIN(0, "Role_Admin"), CLIENTE(1,"Role_Cliente"), TECNICO(2, "Role_Tecnico");
+
+    private final Integer codPerfil;
+    private final String descricao;
+
+    Perfil(Integer codPerfil, String descricao) {
+        this.codPerfil = codPerfil;
+        this.descricao = descricao;
+    }
+
+    public Perfil toEnum(Integer codPerfil) {
+        if (codPerfil == null){
+            return null;
+        }
+
+        for (Perfil perfil : Perfil.values()){
+            if (codPerfil.equals(getCodPerfil())){
+                return perfil;
+            }
+        }
+
+        throw new IllegalArgumentException("Código de Perfil Inválido.");
+    }
+    public Integer getCodPerfil() {
+        return codPerfil;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+}
---
+++ src/main/java/br/com/helpdesk/domain/enums/Status.java
@@ -0,0 +1,4 @@
+package br.com.helpdesk.domain.enums;
+
+public enum Status {
+}
